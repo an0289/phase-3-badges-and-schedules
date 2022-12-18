ee456fb1080fbaa3821dfda01038186f9ebca4d6
Alexandra <alexandraneff@live.com>
2022-12-17 22:26:00 -0600

    commit tree 4b63e8b8fc2e9c7e3affc4d5ab25444857d54841

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker (name)
+ "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator (array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end 
+end 
+
+def assign_rooms (array)
+    array.map.with_index(1) do |name, num|
+        "Hello, #{name}! You'll be assigned to room #{num}!"
+    end 
+end 
+
+def printer (array)
+    batch_badge_creator(array).map do |name|
+        puts name 
+    end 
+    assign_rooms(array).map do |name|
+        puts name 
+    end
+end 
